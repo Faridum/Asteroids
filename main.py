4e960b2a548ea084a9970da784806eb13bdcb665
Faridum <farid.jamaleldin@gmail.com>
2026-06-24 22:06:05 +0300

    Add FPS cap and delta time

--- main.py
+++ main.py
@@ -9,15 +9,21 @@ def main():
         (SCREEN_WIDTH, SCREEN_HEIGHT)
     )
 
+    clock = pygame.time.Clock()
+    dt = 0.0
+
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-            screen.fill("black")
-            pygame.display.flip()
+        screen.fill("black")
+        pygame.display.flip()
 
+        dt = clock.tick(60) / 1000
+        print(dt)
+     
 
 if __name__ == "__main__":
     main()
