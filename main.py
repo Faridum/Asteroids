f13f37ebe960e1329b88bb7096297211cf2b5f3b
Faridum <farid.jamaleldin@gmail.com>
2026-06-25 15:24:50 +0300

    Add player rendering

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
@@ -19,10 +20,15 @@ def main():
                 return
             
         screen.fill("black")
+        player = Player(
+        SCREEN_WIDTH / 2,
+        SCREEN_HEIGHT / 2
+        )
+        player.draw(screen)
+        
         pygame.display.flip()
 
-        dt = clock.tick(60) / 1000
-        print(dt)
+        dt = clock.tick(60) / 1000 
      
 
 if __name__ == "__main__":
