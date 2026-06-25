254d57d38421e599b8541bc4af3657182a1c801a
Faridum <farid.jamaleldin@gmail.com>
2026-06-25 21:49:07 +0300

    Implement player-asteroid collision detection

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import pygame
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+import sys
 
 
 def main():
@@ -46,6 +47,12 @@ def main():
                 return
 
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
         screen.fill("black")
 
